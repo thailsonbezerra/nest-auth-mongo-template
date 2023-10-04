5b2f77ab8fd296541dcc3730ef287507283be80b
Thailson Bezerra <thailsonbezerra@outlook.com>
2023-10-03 23:05:37 -0300

    build: dockerizando o db e o app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://mongodb:27017/login'),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
