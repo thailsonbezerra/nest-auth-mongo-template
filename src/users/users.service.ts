a209164e906679fbdb9df4d20de98208fedbfb6c
Thailson Bezerra <thailsonbezerra@outlook.com>
2023-10-03 23:13:11 -0300

    feat: configurando schema user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './entities/user.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
